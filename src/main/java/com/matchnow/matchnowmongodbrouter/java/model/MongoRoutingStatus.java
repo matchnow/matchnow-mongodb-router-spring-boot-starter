e0eac99a004c5d191b2881aa69d1dfad9e937ead
KangChanSong <thdrkdcks@naver.com>
2024-05-07 13:44:17 +0900

    [CI] 필요없는 코드 제거

--- src/main/java/com/matchnow/matchnowmongodbrouter/java/model/MongoRoutingStatus.java
+++ src/main/java/com/matchnow/matchnowmongodbrouter/java/model/MongoRoutingStatus.java
@@ -2,18 +2,4 @@ package com.matchnow.matchnowmongodbrouter.java.model;
 
 public enum MongoRoutingStatus {
     READ, WRITE;
-
-    private boolean isDefault;
-
-    public boolean isDefault() {
-        return isDefault;
-    }
-
-    void setDefault(boolean aDefault) {
-        isDefault = aDefault;
-    }
-
-    public MongoRoutingStatus defaultStatus() {
-        return READ.isDefault ? READ : WRITE;
-    }
 }
