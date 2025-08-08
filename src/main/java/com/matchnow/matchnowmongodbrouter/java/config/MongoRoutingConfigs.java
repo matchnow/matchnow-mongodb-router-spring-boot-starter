0e9caab9b43ec996ccbf824b622dbe4059011416
KangChanSong <thdrkdcks@naver.com>
2025-08-08 16:39:39 +0900

    [MOD] 로깅 추가

--- src/main/java/com/matchnow/matchnowmongodbrouter/java/config/MongoRoutingConfigs.java
+++ src/main/java/com/matchnow/matchnowmongodbrouter/java/config/MongoRoutingConfigs.java
@@ -9,6 +9,11 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ReadPreference;
 import com.mongodb.client.MongoClient;
+import com.mongodb.event.CommandFailedEvent;
+import com.mongodb.event.CommandListener;
+import com.mongodb.event.CommandStartedEvent;
+import com.mongodb.event.CommandSucceededEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoClientFactory;
@@ -22,7 +27,9 @@ import org.springframework.context.annotation.Primary;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Configuration
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties(MongoProperties.class)
@@ -45,8 +52,14 @@ public class MongoRoutingConfigs {
     @Bean
     @Primary
     public MongoClient mongoRoutingClient(MongoClientSettings settings) {
-        MongoClient writeClient = new MongoClientFactory(List.of(writeCustomizer(writeUri))).createMongoClient(MongoClientSettings.builder().build());
-        MongoClient readClient = new MongoClientFactory(List.of(readCustomizer(readUri))).createMongoClient(MongoClientSettings.builder().build());
+        MongoClient writeClient = new MongoClientFactory(
+                List.of(writeCustomizer(writeUri), addLogging("WRITE", writeUri))
+        ).createMongoClient(MongoClientSettings.builder().build());
+
+        MongoClient readClient = new MongoClientFactory(
+                List.of(readCustomizer(readUri), addLogging("READ", readUri))
+        ).createMongoClient(MongoClientSettings.builder().build());
+
         return new MongoRoutingClient(writeClient, readClient);
     }
 
@@ -68,5 +81,51 @@ public class MongoRoutingConfigs {
     private MongoClientSettingsBuilderCustomizer readCustomizer(String uri) {
         return b -> b.applyConnectionString(new ConnectionString(uri))
                 .readPreference(ReadPreference.secondaryPreferred());   // prefer secondary, fallback OK
+    }
+
+    // 여기서 CommandListener 주입
+    private MongoClientSettingsBuilderCustomizer addLogging(String role, String uriTag) {
+        return builder -> builder.addCommandListener(new LoggingCommandListener(role, uriTag));
+    }
+
+    static class LoggingCommandListener implements CommandListener {
+        private final String role;
+        private final String uriTag;
+
+        LoggingCommandListener(String role, String uriTag) {
+            this.role = role;
+            this.uriTag = uriTag;
+        }
+
+        @Override
+        public void commandStarted(CommandStartedEvent event) {
+            var addr = event.getConnectionDescription().getServerAddress(); // host:port
+
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] Mongo command started: name={}, server={}, uriTag={}",
+                        role, event.getCommandName(), addr, uriTag);
+            }
+
+        }
+
+        @Override
+        public void commandSucceeded(CommandSucceededEvent event) {
+            var addr = event.getConnectionDescription().getServerAddress();
+
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] Mongo command succeeded: name={}, server={}, durationMs={}",
+                        role, event.getCommandName(), addr, event.getElapsedTime(TimeUnit.MILLISECONDS));
+            }
+        }
+
+        @Override
+        public void commandFailed(CommandFailedEvent event) {
+            var addr = event.getConnectionDescription().getServerAddress();
+
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] Mongo command failed: name={}, server={}, error={}",
+                        role, event.getCommandName(), addr, event.getThrowable().getMessage());
+            }
+        }
     }
 }
